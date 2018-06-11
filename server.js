5845fc64f6670567a3115849eddefd0d4f8074ef
Ki Jung Kim <kimkijung@icloud.com>
2018-06-11 13:58:27 -1000

    fixed smoke test to return the response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (request, response) => {
-  response.send('Smoke test');
+  return response.send('Smoke test');
 })
 
 module.exports = app;
