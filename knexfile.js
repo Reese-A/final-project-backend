514c096ae82aab082b671d33af4cad037d4484af
Ki Jung Kim <kimkijung@icloud.com>
2018-06-11 14:24:18 -1000

    Initialized knex and setup development environment

---
+++ knexfile.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      database: 'fitbyte',
+      user: 'fitbyte_admin',
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
