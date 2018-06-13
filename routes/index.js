e04b539c906c63e04b8327812a19eb027eb45b1d
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-12 17:59:00 -1000

    Created routing index file

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// const users = require('./users');
+// const activityLevels = require('./activity_levels');
+// const goals = require('./goals');
+// const foods = require('./foods');
+// const dishes = require('./dishes');
+// const categories = require('./categories');
+
+const router = express.Router();
+
+// router.use('/users', users);
+// router.use('/activitylevels', activityLevels);
+// router.use('/goals', goals);
+// router.use('/foods', foods);
+// router.use('/dishes', dishes);
+// router.use('/categories', categories);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
+app.use('/api', routes);
+
 app.get('*', (request, response) => {
   return response.send('Smoke test');
-})
+});
 
 module.exports = app;
