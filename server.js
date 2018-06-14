1e3ea04aa7e656232a3c0a9cf7269426ed6d358a
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-13 17:18:24 -1000

    Implemented body parser

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 
 app.use('/api', routes);
 
