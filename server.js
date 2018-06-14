9413aaf80584ff77ae13bf6f0d14cc961ba29567
Ki Jung Kim <kimkijung@icloud.com>
2018-06-13 16:16:15 -1000

    added bodyparser

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const routes = require('./routes');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/api', routes);
 
