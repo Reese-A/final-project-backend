6ee014ae3921daff60555f99f4e2aaac54809cb8
Jace Mitsuka <PJace@BigMac.local>
2018-06-19 14:20:15 -1000

    added get and put '/google/token' routes

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,39 +1,38 @@
 const express = require('express');
-const passport = require('passport');
+
 const router = express.Router();
+
 const User = require('../db/models/User');
 
 router.route('/google/token')
 .get((req, res) => {
-  
+  const { id } = req.user;
+  return new User({ id })
+  .fetch()
+  .then(user => {
+    return res.json(user)
+  })
+  .catch(err => {
+    console.log(err); 
+  })
 })
 .put((req, res) => {
-  console.log('req body', req.body);
-  console.log('req user', req);
-  // const { id } = req.user;
-  // return new User({})
-
+  const { access_token } = req.body.tokenObj;
+  const { email } = req.body.profileObj;
+  return new User()
+  .where({ email })
+  .save(
+    { access_token }, 
+    { method: 'update' }
+  )
+  .then(user => {
+    return res.json(user);
+  })
+  .catch(err => {
+    console.log(err);
+    return res.status(500).json(err);
+  })
 });
 
-
-
-
-// router.route('/google/callback')
-// .get((req, res, next) => {
-//   passport.authenticate('google', (err, user, info) => {
-//     console.log('callback err', err);
-//     console.log('callback user', user);
-//     console.log('callback info', info);
-//     if (err) { return next(err) }
-//     if (!user) {
-//       console.log('!user');
-//       console.log('!user req', req.body);
-//       return res.json({ message: info.message })
-//     } else {
-//       console.log('callback user req', user);
-//       return res.json(user);
-//     }
-//   })(req, res, next);
-// })
 
 module.exports = router;
