020d74b7406ce02ed0a6b3349f3125265588720b
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-21 15:52:01 -1000

    Remove name unique and popularity columns

--- db/migrations/20180612101844_add_dishes_table.js
+++ db/migrations/20180612101844_add_dishes_table.js
@@ -1,28 +1,16 @@
-
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable('dishes', (table) => {
-    table
-      .increments();
-    table
-      .string('name')
-      .notNullable()
-      .unique();
-    table
-      .integer('user_id')
-      .notNullable()
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('dishes', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('user_id').notNullable();
     table
       .foreign('user_id')
       .references('id')
       .inTable('users');
-    table
-      .integer('popularity')
-      .notNullable()
-      .defaultTo(0);
-    table
-      .timestamps(true, true);
-  })
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function(knex, Promise) {
   return knex.schema.dropTable('dishes');
 };
