1575678a227eb0af8216f8fb274233679769dad1
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-06-21 15:52:12 -1000

    Add daily consumption table

---
+++ db/migrations/20180620155042_add_daily_consumption_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('daily_consumption', table => {
+    table.increments();
+    table.integer('user_id').notNullable();
+    table
+      .foreign('user_id')
+      .references('id')
+      .inTable('users');
+    table.decimal('total_calories', 9, 3).notNullable();
+    table.specificType('tracked_calories', 'decimal[]');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('daily_consumption');
+};
